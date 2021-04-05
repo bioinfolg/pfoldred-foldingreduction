e7fbae6e0d55598d4d9f74d733f4c1f18b11a363
Luis Garreta <lgarreta@gmail.com>
2021-04-05 10:32:15 -0500

    Updated repo with links

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+# Install pfoldred:
+## 1. Add environment into bashrc
+## 2. Install local R libraries
+
 sed "/PFOLDRED_HOME/d" -i ~/.bashrc
 
 echo "export PFOLDRED_HOME=$PWD" >> ~/.bashrc
